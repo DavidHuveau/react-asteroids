4182fbde11f73f3c5db341eb1686d1bb3fa83f82
david huveau <davidhuveau@mac171even32.eventmaker>
2023-12-07 09:07:18 +0100

    use asteroids game

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
-import AppForCanvas from "./CanvasComponent/AppForCanvas";
+// import AppForCanvas from "./CanvasComponent/AppForCanvas";
 // import AppForCanvas from "./CanvasHook/AppForCanvas";
 // import SpaceShip from "./Exercices/1/SpaceShip";
 // import Asteroid from "./Exercices/1/Asteroid";
 // import DrawWithClasses from "./Exercices/2/DrawWithClasses";
 // import DrawWithMass from "./Exercices/3/DrawWithMass";
 // import ControllingTheStarShip from "./Exercices/4/ControllingTheStarShip";
-// import HandleWithCollisions from "./Exercices/5/HandleWithCollisions";
+import HandleWithCollisions from "./Exercices/5/HandleWithCollisions";
 
 function App() {
   return <div className="App">
@@ -14,13 +14,13 @@ function App() {
       header
     </header>
     <main>
-      <AppForCanvas />
+      {/* <AppForCanvas /> */}
       {/* <SpaceShip /> */}
       {/* <Asteroid /> */}
       {/* <DrawWithClasses /> */}
       {/* <DrawWithMass /> */}
       {/* <ControllingTheStarShip /> */}
-      {/* <HandleWithCollisions /> */}
+      <HandleWithCollisions />
     </main>
   </div>
 }
